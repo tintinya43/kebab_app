ffb511816bf931b6c86b1500438a1a555ae6f95c
EC2 Default User <ec2-user@ip-172-31-9-57.us-east-2.compute.internal>
2020-04-05 03:13:34 +0000

    advenced_login20200405

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,9 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  
+  test "authenticated? should return false for a user with nil digest" do
+    assert_not @user.authenticated?('')
+  end
+
 end
