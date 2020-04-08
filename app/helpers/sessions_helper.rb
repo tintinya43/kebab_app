573b9ff6affddb5c54bcdd722c79231b941862a8
EC2 Default User <ec2-user@ip-172-31-9-57.us-east-2.compute.internal>
2020-04-08 09:40:06 +0000

    updating-users_finish_20200408

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,29 +1,32 @@
 module SessionsHelper
-
   # 渡されたユーザーでログインする
   def log_in(user)
     session[:user_id] = user.id
   end
-  
-  # ユーザーのセッションを永続的にする
+
+  # ユーザーを永続的に復元できるようになった
   def remember(user)
-    user.remember
+    user.remember # => DB: remember_digest
     cookies.permanent.signed[:user_id] = user.id
     cookies.permanent[:remember_token] = user.remember_token
   end
 
-  # 現在ログイン中のユーザーを返す (いる場合)
-  def current_user
-    if session[:user_id]
-      @current_user ||= User.find_by(id: session[:user_id])
-    end
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
   end
-  
-  # 記憶トークンcookieに対応するユーザーを返す
+
+  def current_user?(user)
+    user == current_user
+  end
+
   def current_user
     if (user_id = session[:user_id])
       @current_user ||= User.find_by(id: user_id)
+
     elsif (user_id = cookies.signed[:user_id])
+      # raise
       user = User.find_by(id: user_id)
       if user && user.authenticated?(cookies[:remember_token])
         log_in user
@@ -32,22 +35,25 @@ module SessionsHelper
     end
   end
 
-  # ユーザーがログインしていればtrue、その他ならfalseを返す
   def logged_in?
     !current_user.nil?
   end
-  
-  # 永続的セッションを破棄する
-  def forget(user)
-    user.forget
-    cookies.delete(:user_id)
-    cookies.delete(:remember_token)
-  end
-  
-  # 現在のユーザーをログアウトする
+
+   # 現在のユーザーをログアウトする
   def log_out
     forget(current_user)
     session.delete(:user_id)
     @current_user = nil
+  end
+
+  # 記憶したURL (もしくはデフォルト値) にリダイレクト
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+
+  # アクセスしようとしたURLを覚えておく
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
   end
 end
