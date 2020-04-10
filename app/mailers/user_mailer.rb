686340b7c19e9853add2fe16e70f16da556a3c34
EC2 Default User <ec2-user@ip-172-31-9-57.us-east-2.compute.internal>
2020-04-10 02:13:48 +0000

    add pasdwordreset_20200410

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,11 +4,9 @@ class UserMailer < ApplicationMailer
     @user = user
     mail to: user.email, subject: "Account activation"
   end
-  
-  
-  def password_reset
-    @greeting = "Hi"
 
-    mail to: "to@example.org"
+  def password_reset(user)
+    @user = user
+    mail to: user.email, subject: "Password reset"
   end
 end
