2f910dae1e884a2aaa1b4be7afc3421388af6b84
tintinya43 <tintinya43@icloud.com>
2020-04-24 09:04:32 +0000

    add_ap2

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,9 +4,9 @@ if Rails.env.production?
       # Amazon S3用の設定
       :provider              => 'AWS',
       :region                => ENV['ap-northeast-1'],     # 例: 'ap-northeast-1'
-      :aws_access_key_id     => ENV[AKIA24RVOYR3PXTCAY7J],
-      :aws_secret_access_key => ENV[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]
+      :aws_access_key_id     => ENV['AKIA24RVOYR3PXTCAY7J'],
+      :aws_secret_access_key => ENV['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']
     }
-    config.fog_directory     =  ENV[kebabapp]
+    config.fog_directory     =  ENV['kebabapp']
   end
 end
