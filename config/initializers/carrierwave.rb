e4381e99cbb21ea2224c54e8894f1fba48160074
tintinya43 <tintinya43@icloud.com>
2020-04-24 07:14:45 +0000

    20200424

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,9 +3,9 @@ if Rails.env.production?
     config.fog_credentials = {
       # Amazon S3用の設定
       :provider              => 'AWS',
-      :region                => ENV['S3_REGION'],     # 例: 'ap-northeast-1'
-      :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
-      :aws_secret_access_key => ENV['S3_SECRET_KEY']
+      :region                => ENV['ap-northeast-1'],     # 例: 'ap-northeast-1'
+      :aws_access_key_id     => ENV['AKIA24RVOYR3PXTCAY7J'],
+      :aws_secret_access_key => ENV['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']
     }
     config.fog_directory     =  ENV['S3_BUCKET']
   end
