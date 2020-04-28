0acc23a0b399df4abe959e018abf0174edd272bb
tintinya43 <tintinya43@icloud.com>
2020-04-28 10:37:33 +0000

    add bucketname

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -8,7 +8,7 @@ class Micropost < ApplicationRecord
 
   private
 
-    # アップロードされた画像のサイズをバリデーションする
+  
     def picture_size
       if picture.size > 5.megabytes
         errors.add(:picture, "should be less than 5MB")
