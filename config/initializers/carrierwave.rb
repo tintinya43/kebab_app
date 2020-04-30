92f55ab47a8ab33d699d386d7beb9fe5e789c40d
tintinya43 <tintinya43@icloud.com>
2020-04-30 12:05:22 +0000

    add

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,7 +1,7 @@
 if Rails.env.production?
   CarrierWave.configure do |config|
     config.fog_credentials = {
-      # Amazon S3用の設定
+      # Amazon S3用の設
       :provider              => 'AWS',
       :region                => ENV['ap-northeast-1'],     
       :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
