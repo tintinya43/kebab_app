f07213121c05f5e819bb62ae842da1ea6d5f401a
tintinya43 <tintinya43@icloud.com>
2020-05-01 04:27:40 +0000

    change carrirwave2

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,12 +1,17 @@
 if Rails.env.production?
   CarrierWave.configure do |config|
+    config.fog_provider = 'fog/aws' 
     config.fog_credentials = {
-      # Amazon S3用の設
+      # Amazon S3用の設定
       :provider              => 'AWS',
-      :region                => ENV['S3_REGION'],     
+      :region                => ENV['S3_REGION'], 
       :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
       :aws_secret_access_key => ENV['S3_SECRET_KEY']
     }
     config.fog_directory     =  ENV['S3_BUCKET']
+    config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" } 
   end
+
+  # 日本語ファイル名の設定
+  CarrierWave::SanitizedFile.sanitize_regexp = /[^[:word:]\.\-\+]/ 
 end
