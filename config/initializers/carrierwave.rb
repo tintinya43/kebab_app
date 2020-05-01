625a3b20fa4dfc86693668eab1ef2ee9cf704fd2
tintinya43 <tintinya43@icloud.com>
2020-05-01 03:18:41 +0000

    change carrirwave

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,10 +3,10 @@ if Rails.env.production?
     config.fog_credentials = {
       # Amazon S3用の設
       :provider              => 'AWS',
-      :region                => ENV['ap-northeast-1'],     
+      :region                => ENV['S3_REGION'],     
       :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
       :aws_secret_access_key => ENV['S3_SECRET_KEY']
     }
-    config.fog_directory     =  ENV['kebabapp3']
+    config.fog_directory     =  ENV['S3_BUCKET']
   end
 end
