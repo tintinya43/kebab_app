f616c9254744ff8e23b8cdaac25580f55ce03ba4
tintinya43 <tintinya43@icloud.com>
2020-05-06 05:03:15 +0000

    Add user following20200507

--- config/application.rb
+++ config/application.rb
@@ -15,4 +15,6 @@ module Kebab
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
   end
+  # 認証トークンをremoteフォームに埋め込む
+ #   config.action_view.embed_authenticity_token_in_remote_forms = true
 end
